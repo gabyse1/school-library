1a8152d0e2fe7459fcf3a97bf51e5c284ae8997e
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-11 16:19:20 -0500

    Create the Teacher class which inherits properties and methods from Person class

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor :spetialization
+
+  def initialize(spetialization, age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
+    @spetialization = spetialization
+  end
+
+  def can_use_service?
+    true
+  end
+end
