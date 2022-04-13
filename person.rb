ad165b2ed7b036442bacc6ff6af5474ea7976bfe
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-13 18:00:41 -0500

    Update persn class to add its has-many relationship with the Rental class

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true, **options)
     super(**options)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_service?
